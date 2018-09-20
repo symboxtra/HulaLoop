7abc606fd10d403a20bbb67cf8369a51fda59313
Neel Patel <patelneel55@gmail.com>
2018-09-20 16:37:53 -0400

    Get system audio devices for Windows

--- src/os/WindowsAudio.cpp
+++ src/os/WindowsAudio.cpp
@@ -1,31 +1,104 @@
 #include "WindowsAudio.h"
 
-WindowsAudio::setBufferSize(size_t size)
+WindowsAudio::WindowsAudio()
+{
+
+}
+
+void WindowsAudio::setBufferSize(uint32_t size)
 {
     this->captureBufferSize = size;
 }
 
-WindowsAudio::addBufferReadyCallback(f_int_t callFunction)
+void WindowsAudio::addBufferReadyCallback(f_int_t callFunction)
 {
     this->callbackList.push_back(callFunction);
 }
 
-WindowsAudio::getInputDevices()
+vector<Device*> WindowsAudio::getAudioDevices()
 {
+    deviceList = NULL;
 
-}
-
-WindowsAudio::getOutputDevices()
-{
+    vector<Device*> temp = getInputDevices();
+    if(temp != NULL)
+        deviceList.insert(deviceList.end(), temp.begin(), temp.end());
     
+    temp = getOutputDevices();
+    if(temp != NULL)
+        deviceList.insert(deviceList.end(), temp.begin(), temp.end());
+
+    return deviceList;
 }
 
-WindowsAudio::setActiveOutputDevice(Device* device)
+vector<Device*> WindowsAudio::getInputDevices()
+{
+    vector<Device*> temp = NULL;
+
+    uint deviceCount = waveInGetNumDevs();
+    if(deviceCount > 0)
+    {
+        for(int i = 0;i < deviceCount;i++)
+        {
+            WAVEINCAPSW waveInCaps;
+            waveInGetDevCapsW(i, &waveInCaps, sizeof(WAVEINCAPS));
+
+            temp.push_back(new Device()); //TODO: Figure out Device Class
+        }
+    }
+
+    return temp;
+}
+
+vector<Device*> WindowsAudio::getOutputDevices()
+{
+    vector<Device*> temp = NULL;
+
+    // Create instance? don't exactly know what this does yet
+    status = CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL, IID_IMMDeviceEnumerator, (void**)&pEnumerator);
+    HANDLE_ERROR(status);
+
+    // Get Device Collection
+    status = pEnumerator->EnumAudioEndpoints(eCapture, eConsole, &deviceCollection);
+    HANDLE_ERROR(status);
+
+    // Store all devices as Device instances into vector
+    int count = 0;
+    status = deviceCollection->GetCount(&count);
+    HANDLE_ERROR(status);
+
+    IMMDevice* pp = NULL;
+    for(int i = 0;i < count;i++)
+    {
+        pp = NULL;
+        DWORD state = NULL;
+
+        status = deviceCollection->Item(i, &pp);
+        HANDLE_ERROR(status);
+
+        status = pp->GetState(&state);
+        HANDLE_ERROR(status);
+
+        if(state == DEVICE_STATE_ACTIVE)
+            // Add device to devicelist
+            temp.push_back(new Device()) //TODO: Figure out Device Class
+    }
+
+Exit:
+    SAFE_RELEASE(pEnumerator)
+    SAFE_RELEASE(pp)
+
+    if(FAILED(hres))
+        return NULL;
+    else
+        return temp;
+}
+
+void WindowsAudio::setActiveOutputDevice(Device* device)
 {
     this->activeOutputDevice = device;
 }
 
-WindowsAudio::setActiveRecordDevice(Device* device)
+void WindowsAudio::setActiveRecordDevice(Device* device)
 {
     this->activeInputDevice = device;
 }
--- src/os/WindowsAudio.h
+++ src/os/WindowsAudio.h
@@ -5,26 +5,61 @@
 
 #include <string>
 #include <vector>
+#include <windows.h>
+#include <tchar.h>
 
 using byte = uint8_t; 
 
 using f_int_t = int(*)(int);
 
+#define REFTIMES_PER_SEC  10000000
+#define REFTIMES_PER_MILLISEC  10000
+
+#define EXIT_ON_ERROR(hres) \
+            if (FAILED(hrs)) { goto Exit; }
+#define SAFE_RELEASE(punk) \
+            if ((punk) != NULL) \
+                { (punk)->Release(); (punk) = NULL; }
+
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+
 class WindowsAudio {
 
 private:
+
+    //TODO: Figure out device class
+
     Device* activeOutputDevice;
     Device* activeInputDevice;
-    size_t captureBufferSize;
+    uint32_t captureBufferSize;
     vector<f_int_t> callbackList;
+
+    vector<Device*> deviceList;
+
+    // System necessary variables
+    HRESULT status;
+    REFERENCE_TIME requestDuration = REFTIMES_PER_SEC;
+    REFERENCE_TIME bufferDuration;
+
+    IMMDeviceEnumerator *pEnumerator = NULL;
+    IMMDeviceCollecton *deviceCollection = NULL;
+    IAudioClient *pAudioClient = NULL;
+    IAudioCaptureClient *pCaptureClient = NULL;
+    WAVEFORMATEX *pwfx = NULL;
+
 
 
 public:
 
-    Device* getInputDevices();
-    Device* getOutputDevices();
+    WindowsAudio();
+    ~WindowsAudio();
 
-    void setBufferSize(size_t size);
+    vector<Device*> getAudioDevices();
+    vector<Device*> getInputDevices();
+    vector<Device*> getOutputDevices();
+
+    void setBufferSize(uint32_t size);
 
     void setActiveRecordDevice(Device* device);
     void setActiveOutputDevice(Device* device);
