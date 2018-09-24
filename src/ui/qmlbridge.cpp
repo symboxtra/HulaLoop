d60bcb9d2cec03a75ac6637eece237ab4e06b8af
Aneesh Gokhale <dev.agokhale1@gmail.com>
2018-09-23 21:10:46 -0400

    Implement C++/QML bridge

---
+++ src/ui/qmlbridge.cpp
@@ -0,0 +1,19 @@
+#include "qmlbridge.h"
+
+QMLBridge::QMLBridge(QObject *parent) : QObject(parent) {}
+
+QString QMLBridge::getState()
+{
+	return state;
+}
+
+void QMLBridge::setState(const QString &state)
+{
+
+	if(this->state == state)
+		return;
+
+	this->state = state;
+	emit stateChanged();
+
+}
