ea24a3aefbe791bd3c95375c2f36e7befb4fe57c
Jack McKernan <jmcker@outlook.com>
2018-10-03 14:59:22 -0400

    Add doxygen to conf.py

--- docs/read-the-docs/source/conf.py
+++ docs/read-the-docs/source/conf.py
@@ -16,7 +16,14 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess, os
 from recommonmark.parser import CommonMarkParser
+
+# -- Doxygen build -----------------------------------------------------
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+if read_the_docs_build:
+    subprocess.call('cd ../../; doxygen', shell=True)
 
 
 # -- Project information -----------------------------------------------------
