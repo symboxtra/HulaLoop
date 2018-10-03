2c6c7ac2a69446b74dc94f4acc29d488a67ac92a
Meriem <mbounab@purdue.edu>
2018-10-03 12:48:59 -0400

    Wrap install script paths in quotes

--- src/audio/OSXDriver/install.sh
+++ src/audio/OSXDriver/install.sh
@@ -7,11 +7,11 @@ echo "Copying build output to HAL Plug-In folder..."
 echo "IMPORTANT: This requires root permissions."
 echo "           If xcodebuild hangs, it's waiting for root password."
 echo "           Interrupt it and run it with sudo, or copy the files yourself using:"
-echo "    sudo cp -rf ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_NAME}.driver ${LOCAL_LIBRARY_DIR}/Audio/Plug-Ins/HAL"
+echo "    sudo cp -rf \"${BUILT_PRODUCTS_DIR}/${EXECUTABLE_NAME}.driver\" \"${LOCAL_LIBRARY_DIR}/Audio/Plug-Ins/HAL\""
 echo
 
 # Copy the driver to the HAL folder
-$(set -x && sudo cp -rf ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_NAME}.driver ${LOCAL_LIBRARY_DIR}/Audio/Plug-Ins/HAL)
+$(set -x && sudo cp -rf "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_NAME}.driver" "${LOCAL_LIBRARY_DIR}/Audio/Plug-Ins/HAL")
 
 echo "Restarting CoreAudio service..."
 
