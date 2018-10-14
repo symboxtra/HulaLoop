56c5c96dfdd2accba7fd32f20b9af0e45bde649b
epituch <epituch@purdue.edu>
2018-10-14 16:55:43 -0400

    fixed shell file

--- apply-style.sh
+++ apply-style.sh
@@ -11,11 +11,12 @@ export -f runAstyle
 if [ $# -eq 0 ] || [ "$1" == "-h" ] || [ "$1" == "help" ]; then
     echo "Usage: apply-style.sh file.cpp file2.cpp ..."
     echo "       apply-style.sh *.cpp"
+    echo "       apply-style.sh all"
     exit 0
 fi
 
 if [ "$1" == "all" ]; then
-    find . \( -path ./src/libs -o -path ./src/audio/OSXDriver -o -path ./build* \) -prune \
+    find . \( -path './src/libs' -o -path './src/audio/OSXDriver' -o -path './build*' \) -prune \
     -o \( -name "*.cpp" -o -name "*.h" \) -exec bash -c 'runAstyle "$0"' {} \;
     exit $?
 fi
