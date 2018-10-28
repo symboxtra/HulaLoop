359cdfcfca2fa09f34827d8788abbe9a2c53655d
epituch <epituch@purdue.edu>
2018-10-28 16:18:54 -0400

    Changed tabs to spaces in makeMainMan.sh

--- docs/makeMainMan.sh
+++ docs/makeMainMan.sh
@@ -1,15 +1,15 @@
 # Check args, if there is not any then just default to build path
 if [ $# -eq 0 ]; then
-	pathToExec="../build/bin/hulaloop-cli"
+    pathToExec="../build/bin/hulaloop-cli"
 else
-	pathToExec=$1
+    pathToExec=$1
 fi
 # Run help2man
 help2man $pathToExec > hulaloop.1
 # Check errors in running help2man
 if [ $? -eq 1 ]; then
-	echo "Error running help2man exiting..."
-	exit 1
+    echo "Error running help2man exiting..."
+    exit 1
 fi
 # Gzip because that is the format of the man page
 gzip hulaloop.1
