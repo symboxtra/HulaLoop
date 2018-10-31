3113459290d9af883ae3f12ef105b4717dd4e7bd
Jack McKernan <jmcker@outlook.com>
2018-10-31 01:32:18 -0400

    Add memory management Doxygen page. Closes #146

--- src/audio/include/hlaudio/hlaudio.h
+++ src/audio/include/hlaudio/hlaudio.h
@@ -5,4 +5,12 @@
 #include "hlaudio/internal/HulaAudioError.h"
 #include "hlaudio/internal/HulaRingBuffer.h"
 
+/**
+ * \defgroup memory_management Memory Management
+ * Commands related to any memory management required of library end-users.
+ *
+ * A number of methods in the HulaLoop API return objects that must
+ * be deleted by the caller. Each of these is detailed below.
+ */
+
 #endif // __HL_AUDIO_H__
