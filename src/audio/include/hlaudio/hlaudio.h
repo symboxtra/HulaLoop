6cd4b99a856b60a3db3486b957e1379cd2eeb412
Jack McKernan <jmcker@outlook.com>
2018-10-31 13:18:24 -0400

    Update Doxygen pages

--- src/audio/include/hlaudio/hlaudio.h
+++ src/audio/include/hlaudio/hlaudio.h
@@ -1,16 +1,13 @@
 #ifndef __HL_AUDIO_H__
 #define __HL_AUDIO_H__
 
+/**
+ * @file hlaudio.h <hlaudio/hlaudio.h>
+ * @ingroup public_api
+ */
+
 #include "hlaudio/internal/Controller.h"
 #include "hlaudio/internal/HulaAudioError.h"
 #include "hlaudio/internal/HulaRingBuffer.h"
-
-/**
- * \defgroup memory_management Memory Management
- * Commands related to any memory management required of library end-users.
- *
- * A number of methods in the HulaLoop API return objects that must
- * be deleted by the caller. Each of these is detailed below.
- */
 
 #endif // __HL_AUDIO_H__
