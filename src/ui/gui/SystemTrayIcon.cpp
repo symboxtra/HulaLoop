4ad06e9b3570925cdd7d108f1f6ca566d42cb69f
Aneesh Gokhale <dev.agokhale1@gmail.com>
2018-10-31 23:21:13 -0400

    Add comments for Doxygen

--- src/ui/gui/SystemTrayIcon.cpp
+++ src/ui/gui/SystemTrayIcon.cpp
@@ -4,12 +4,20 @@
 #include <QApplication>
 #include <QIcon>
 
+/**
+ * Construct a new instance of the SystemTrayIcon class.
+ *
+ * @param parent The parent object of SystemTrayIcon
+ */
 SystemTrayIcon::SystemTrayIcon(QObject *parent) : QObject(parent)
 {
     trayIcon = new QSystemTrayIcon(QIcon(":/res/hulaloop-logo.svg"), this);
     trayIconMenu = new QMenu();
 }
 
+/**
+ * Initializes the system tray icon with a custom menu.
+ */
 void SystemTrayIcon::init()
 {
 
@@ -40,21 +48,38 @@ void SystemTrayIcon::init()
 
 }
 
+/**
+ * Displays a balloon message with the provided title and message to the user.
+ *
+ * @param title - Balloon message title
+ * @param msg - Content of balloon message
+ */
 void SystemTrayIcon::showMessage(const QString &title, const QString &msg)
 {
     trayIcon->showMessage(title, msg, QIcon(":/res/hulaloop-logo.png"), 10000);
 }
 
+/**
+ * Changes the system tray icon tooltip text.
+ *
+ * @param text - The system tray icon's tooltip text
+ */
 void SystemTrayIcon::setToolTip(const QString &text)
 {
     trayIcon->setToolTip(text);
 }
 
+/**
+ * Changes the system tray icon to the default image.
+ */
 void SystemTrayIcon::setDefaultIcon()
 {
     trayIcon->setIcon(QIcon(":/res/hulaloop-logo.svg"));
 }
 
+/**
+ * Changes the system tray icon to the recording image.
+ */
 void SystemTrayIcon::setRecordIcon()
 {
     trayIcon->setIcon(QIcon(":/res/hulaloop-logo-recording.svg"));
